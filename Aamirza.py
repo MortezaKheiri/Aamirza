b249a8f9d6871e8a9aadd63de751d5871542e819
Morteza Kheiri <morteza.kheiri@gmail.com>
2023-09-05 02:53:41 +0330

    Aamirza.py added to repository

---
+++ Aamirza.py
@@ -0,0 +1,39 @@
+from itertools import permutations
+from hazm import Normalizer, word_tokenize
+import pandas as pd
+
+dictionary = pd.read_fwf("C:/Users/Morteza Kheiri/Downloads/Moin_Key_Words.txt")
+
+def is_meaningful(word):
+    # Implement a function to check if a word is meaningful in Persian
+    # You can use a pre-built dictionary or an API to check for meaningful words
+    if(list(dictionary['اب']).count(word) > 0):
+        return True
+    else :
+        return False
+    
+def get_meaningful_permutations(input_string):
+    normalizer = Normalizer()
+    normalized_input = normalizer.normalize(input_string)
+    words = word_tokenize(normalized_input)
+    
+    meaningful_permutations = set()
+    for word in words:
+        for length in range(1, len(word) + 1):
+            for perm in permutations(word, length):
+                perm_word = ''.join(perm)
+                if is_meaningful(perm_word):
+                    meaningful_permutations.add(perm_word)
+    
+    return meaningful_permutations
+
+input_string = input("Letters = ")
+wordLength = int(input("Length = "))
+
+while wordLength > 0 :
+    result = get_meaningful_permutations(input_string)
+
+    for r in result :
+        if(len(r) == wordLength):
+            print(r)
+    wordLength = int(input("Length = "))
